4ea5a09ea87e601c1c05916b768794142db8c38e
sara <smhumphries@hotmail.co.uk>
2021-06-04 14:11:00 +0100

    owner tested attributes

--- vet_app/tests/owners_tests.py
+++ vet_app/tests/owners_tests.py
@@ -4,5 +4,10 @@ from models.owner import Owner
 class OwnerTest(unittest.TestCase):
 
     def setUp(self):
-        self.owner = Owner("Sara", "Humphries", "01234", "21, Millar Crescent")
-        
+        self.owner1 = Owner("Sara", "Humphries", "01234", "21, Millar Crescent")
+
+    def test_owner_first_name(self):
+        self.assertEqual("Sara", self.owner1.first_name)
+
+    def test_owner_last_name(self):
+        self.assertEqual("Humphries", self.owner1.last_name)
