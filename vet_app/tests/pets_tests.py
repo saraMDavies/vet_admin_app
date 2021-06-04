1449407e5d8e4a90e20617dcd6bfbb010820b4ac
sara <smhumphries@hotmail.co.uk>
2021-06-04 14:19:44 +0100

    pet attrubutes tested

--- vet_app/tests/pets_tests.py
+++ vet_app/tests/pets_tests.py
@@ -8,3 +8,22 @@ class PetTest(unittest.TestCase):
         self.owner1 = Owner("Sara", "Humphries", "01234", "21, Millar Crescent")
         self.vet1 = Vet("Anna", "Hall")
         self.pet1 = Pet("Squeak", "13/04/2020", "Small animal", self.owner1, self.vet1, "good rat")
+
+    def test_pet_has_name(self):
+        self.assertEqual("Squeak", self.pet1.name)
+
+    def test_pet_has_dob(self):
+        self.assertEqual("13/04/2020", self.pet1.dob)
+
+    def test_pet_has_animal_category(self):
+        self.assertEqual("Small animal", self.pet1.animal_category)
+
+    def test_pet_has_owner(self):
+        self.assertEqual(self.owner1, self.pet1.owner)
+
+    def test_pet_has_vet(self):
+        self.assertEqual(self.vet1, self.pet1.vet)
+
+    def test_pet_has_notes(self):
+        self.assertEqual("good rat", self.pet1.notes)
+        
