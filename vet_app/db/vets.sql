0b7b449a10f9f4618e1b9676893e351e65bab515
sara <smhumphries@hotmail.co.uk>
2021-06-04 14:34:04 +0100

    seed data in sql file

--- vet_app/db/vets.sql
+++ vet_app/db/vets.sql
@@ -28,4 +28,18 @@ CREATE TABLE pets(
 );
 
 
+-- seed data
+
+INSERT INTO vets (id, first_name, last_name) VALUES (1, 'Anna', 'Hall')
+INSERT INTO vets (id, first_name, last_name) VALUES (2, 'Bill', 'Oddie')
+INSERT INTO vets (id, first_name, last_name) VALUES (3, 'Carl', 'Driver')
+INSERT INTO vets (id, first_name, last_name) VALUES (4, 'Deena', 'Moore')
+
+INSERT INTO owners (id, first_name, last_name, telephone, address) VALUES (1, "Sara", "Humphries", "01234", "21, Millar Crescent")
+INSERT INTO owners (id, first_name, last_name, telephone, address) VALUES (2, "Alex", "Davies", "07756443245 ", "28, Millar Crescent")
+INSERT INTO owners (id, first_name, last_name, telephone, address) VALUES (3, "Judy", "Garland", "0776889542", "45, Maple Drive")
+
+INSERT INTO pets (id, name, animal_category, notes, vet_id, owner_id) VALUES (1, "Squeak", "Small animal", "good rat", 2, 1)
+INSERT INTO pets (id, name, animal_category, notes, vet_id, owner_id) VALUES (2, "Woof", "Dog", "good boy dog", 1, 3)
+INSERT INTO pets (id, name, animal_category, notes, vet_id, owner_id) VALUES (3, "Wombat", "Small animal", "another good rat, brother of Squeak", 2, 2)
 
