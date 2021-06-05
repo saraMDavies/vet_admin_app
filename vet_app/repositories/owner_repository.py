db1d6e6ba4d24ea8c6745f89b432dbea9b43b302
sara <smhumphries@hotmail.co.uk>
2021-06-05 10:52:15 +0100

    adds get pets function to owners

--- vet_app/repositories/owner_repository.py
+++ vet_app/repositories/owner_repository.py
@@ -2,6 +2,9 @@
 from unittest import result
 from db.run_sql import run_sql
 from models.owner import Owner
+from models.pet import Pet
+from models.vet import Vet
+from repositories import vet_repository
 
 def select_by_id(id):
     owner = None
@@ -33,5 +36,18 @@ def select_by_id(id):
     if result is not None:
         owner = Owner(result['first_name'], result['last_name'], result['telephone'], result['address'], result['id'])
     return owner
+
+def get_all_pets(id):
+    pets = []
+    sql = " SELECT p.* FROM owners o JOIN pets p ON p.owner_id = o.id WHERE o.id = %s"
+    values = [id]
+    results = run_sql(sql, values)
+
+    for row in results:
+        vet = vet_repository.select_by_id(row['vet_id'])
+        owner = select_by_id(row['owne_id'])
+        pet = Pet(row['name'], row['dob'], row['animal_category'], owner, vet, row['notes'], row['id'])
+    return pet
+
 
 
