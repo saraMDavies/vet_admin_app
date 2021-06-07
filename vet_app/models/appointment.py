bcc1d9fee682beafefa64a5f5ee6028adcf004c0
sara <smhumphries@hotmail.co.uk>
2021-06-07 10:57:08 +0100

    Appointment model passes attribute tests

--- vet_app/models/appointment.py
+++ vet_app/models/appointment.py
@@ -1,11 +1,10 @@
 
 class Appointment:
-    def __init__(self, start_time, end_time, description, vet, pet, id=None):
+    def __init__(self, date, start_time, description, vet, pet, id=None):
+        self.date = date
         self.start_time = start_time
-        self.end_time = end_time
         self.description = description
         self.vet = vet
         self.pet = pet
         self.id = id
 
-        
--- vet_app/tests/appointment_tests.py
+++ vet_app/tests/appointment_tests.py
@@ -1,12 +1,29 @@
 import unittest 
 from models.vet import Vet
 from models.pet import Pet
+from models.owner import Owner
 from models.appointment import Appointment
+import datetime
 
 class AppointmentTest(unittest.TestCase):
 
     def setUp(self):
         self.vet1 = Vet("Anna", "Hall")
+        self.owner1 = Owner("Sara", "Humphries", "01234", "21, Millar Crescent")
         self.pet1 = Pet("Squeak", "13/04/2020", "Small animal", self.owner1, self.vet1, "good rat")
-        self.appointment1 = Appointment("13:00", "14:00", "check up", self.vet1, self.pet1)
-        
+        self.appointment1 = Appointment(datetime.date(2021, 7, 13), datetime.time(13, 00), "check up", self.vet1, self.pet1)
+
+    def test_appointment_has_date(self):
+        self.assertEqual(datetime.date(2021, 7, 13), self.appointment1.date)
+
+    def test_appointment_has_time(self):
+        self.assertEqual(datetime.time(13, 00), self.appointment1.start_time)
+    
+    def test_has_description(self):
+        self.assertEqual("check up", self.appointment1.description)
+
+    def test_has_vet(self):
+        self.assertEqual("Anna", self.appointment1.vet.first_name)
+
+    def test_has_pet(self):
+        self.assertEqual("Squeak", self.appointment1.pet.name)
