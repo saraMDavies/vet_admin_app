b0db4e3f5d1c3e272bad73a50f2148832d0137de
sara <smhumphries@hotmail.co.uk>
2021-06-07 13:38:28 +0100

    changes time to integer

--- vet_app/console.py
+++ vet_app/console.py
@@ -1,3 +1,4 @@
+
 from repositories.owner_repository import select_by_id
 from models.pet import Pet
 from models.vet import Vet
@@ -6,11 +7,13 @@ from models.appointment import Appointment
 from repositories import pet_respository, appointment_repository, vet_repository
 import datetime
 
-owner1 = Owner("Sara", "Humphries", "01234", "21, Millar Crescent")
-vet1 = Vet("Anna", "Hall")
-pet1 = Pet("Squeak", "13/04/2020", "Small animal", owner1, vet1, "good rat")
-vet_repository.create(vet1)
-pet_respository.create(pet1)
+
+# owner1 = Owner("Sara", "Humphries", "01234", "21, Millar Crescent")
+# vet1 = Vet("Anna", "Hall")
+# pet1 = Pet("Squeak", "13/04/2020", "Small animal", owner1, vet1, "good rat")
+
+# vet_repository.create(vet1)
+# pet_respository.create(pet1)
 
 # pet = pet_respository.select_by_id(13)
 # print(pet.name)
@@ -22,13 +25,13 @@ pet_respository.create(pet1)
 vet2 = vet_repository.select_by_id(4)
 pet2 = pet_respository.select_by_id(45)
 
-appointment2 = Appointment(datetime.date(2021, 6, 23), "15:00", "check up", vet2, pet2)
+appointment2 = Appointment(datetime.date(2021, 6, 23), 15, "check up", vet2, pet2)
 appointment_repository.create(appointment2)
 
 vet3 = vet_repository.select_by_id(2)
 pet3 = pet_respository.select_by_id(120)
 
-appointment2 = Appointment(datetime.date(2021, 6, 15), "09:00", "vaccinations", vet3, pet3)
+appointment2 = Appointment(datetime.date(2021, 6, 15), 9, "vaccinations", vet3, pet3)
 appointment_repository.create(appointment2)
 
 results = appointment_repository.select_all()
