618290406df8e9e0a307280447a90cb2e9eeb7fd
sara <smhumphries@hotmail.co.uk>
2021-06-08 19:14:54 +0100

    adds diary function through calendar

--- vet_app/models/calendar.py
+++ vet_app/models/calendar.py
@@ -4,6 +4,32 @@ class Calendar:
         self.appointments = appointments
 
     def appointments_by_day(self):
+        diary = []
+        for appointment in self.appointments:
+            placed = False
+            for day in diary:
+                if appointment.date == day['date']:
+                    day['appointments'].append(appointment)
+                    placed = True
+            if not placed:
+                day = {
+                'date': appointment.date,
+                'appointments': [appointment]
+                }
+                diary.append(day)
+
+        ordered_diary = sorted(diary, key=lambda k: k['date'])
+        return ordered_diary
+
+                
+    
+            
+
+
+
+
+
+
         return [{
             'date': self.appointments[0].date,
             'appointments': [self.appointments[0]]
--- vet_app/tests/calendar_tests.py
+++ vet_app/tests/calendar_tests.py
@@ -12,6 +12,9 @@ class CalendarTest(unittest.TestCase):
         self.vet1 = Vet("Anna", "Hall")
         self.pet1 = Pet("Squeak", "13/04/2020", "Small animal", self.owner1, self.vet1, "good rat") 
         self.appointment1 = Appointment(datetime.date(2021,3,14), 4, "check up", self.vet1, self.pet1, True)
+        self.appointment2 = Appointment(datetime.date(2021,4,4), 4, "check up", self.vet1, self.pet1, True)
+        self.appointment3 = Appointment(datetime.date(2021,4,4), 4, "check down", self.vet1, self.pet1, True)
+
         appointments = [self.appointment1]
         self.calendar = Calendar(appointments)
 
@@ -23,4 +26,45 @@ class CalendarTest(unittest.TestCase):
             'date': datetime.date(2021,3,14),
             'appointments' : [self.appointment1]
         }] ,self.calendar.appointments_by_day())
+
+    def test_calender_creates_appointments_by_day__two_app_diffday(self):
+        calendar = Calendar([self.appointment1, self.appointment2])
+        self.assertEqual([{
+            'date': datetime.date(2021,3,14),
+            'appointments' : [self.appointment1]
+            },
+            {
+            'date': datetime.date(2021,4,4),
+            'appointments': [self.appointment2]
+
+            }
+            ], calendar.appointments_by_day())
+    
+
+
+    def test_calender_creates_appointments_by_day__multi_app_sameday(self):
+        calendar = Calendar([self.appointment1, self.appointment2, self.appointment3])
+        self.assertEqual([{
+            'date': datetime.date(2021,3,14),
+            'appointments' : [self.appointment1]
+            },
+            {
+            'date': datetime.date(2021,4,4),
+            'appointments': [self.appointment2, self.appointment3]
+
+            }
+            ], calendar.appointments_by_day())
+
+    def test_calender_creates_appointments_by_day__order(self):
+        calendar = Calendar([self.appointment2, self.appointment3, self.appointment1])
+        self.assertEqual([{
+            'date': datetime.date(2021,3,14),
+            'appointments' : [self.appointment1]
+            },
+            {
+            'date': datetime.date(2021,4,4),
+            'appointments': [self.appointment2, self.appointment3]
+
+            }
+            ], calendar.appointments_by_day())
     
